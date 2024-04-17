861d671e09a9a7eee23d1dc908ef7b0b0efcbd45
Sagar <vadneresagar@gmail.com>
2024-04-17 23:01:10 +0530

    Moved the filter section to a new component file

--- app/product-search/page.tsx
+++ app/product-search/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import ProductCard from '@/src/components/productSearch/ProductCard';
+import FilterSection from '@/src/components/productSearch/FilterSection';
 
 export default function ProductSearch() {
   const [query, setQuery] = useState('');
@@ -77,116 +78,21 @@ export default function ProductSearch() {
       ) : (
         <>
           {results.length != 0 && (
-            <div className='flex flex-wrap -mx-3 mb-2'>
-              <div className='w-full md:w-1/6 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-white-700 text-xs font-bold mb-2'
-                  htmlFor='grid-city'
-                >
-                  Category
-                </label>
-                <div className='relative'>
-                  <select
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-2 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"'
-                  >
-                    <option value=''>All Categories</option>
-                    <option value='Games'>Games</option>
-                    <option value='Outdoors'>Outdoors</option>
-                    <option value='Toys'>Toys</option>
-                    <option value='Garden'>Garden</option>
-                    <option value='Grocery'>Grocery</option>
-                    <option value='Sports'>Sports</option>
-                    <option value='Books'>Books</option>
-                    <option value='Computers'>Computers</option>
-                    <option value='Baby'>Baby</option>
-                    {/* Add more category options */}
-                  </select>
-                  <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
-                    <svg className='fill-current h-4 w-4' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-                      <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-
-              <div className='w-full md:w-1/6 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-white-700 text-xs font-bold mb-2'
-                  htmlFor='grid-city'
-                >
-                  Min Price
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-city'
-                  type='number'
-                  placeholder='100'
-                  value={minPrice}
-                  onChange={(e) => setMinPrice(e.target.value)}
-                />
-              </div>
-              <div className='w-full md:w-1/6 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-white-700 text-xs font-bold mb-2'
-                  htmlFor='grid-city'
-                >
-                  Max Price
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-city'
-                  type='number'
-                  placeholder='500'
-                  value={maxPrice}
-                  onChange={(e) => setMaxPrice(e.target.value)}
-                />
-              </div>
-              <div className='w-full md:w-1/6 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-white-700 text-xs font-bold mb-2'
-                  htmlFor='grid-city'
-                >
-                  Min Reviews
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-city'
-                  type='number'
-                  placeholder='10'
-                  value={minReviews}
-                  onChange={(e) => setMinReviews(e.target.value)}
-                />
-              </div>
-
-              <div className='w-full md:w-1/6 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-white-700 text-xs font-bold mb-2'
-                  htmlFor='grid-zip'
-                >
-                  Min Rating
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-zip'
-                  type='number'
-                  placeholder='3'
-                  value={minRating}
-                  onChange={(e) => setMinRating(e.target.value)}
-                />
-              </div>
-
-              <div className='w-full md:w-1/6 px-3 mb-6 md:mb-0 flex items-end'>
-                <button
-                  onClick={handleButtonClick}
-                  value={query}
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex-1'
-                >
-                  Search
-                </button>
-              </div>
-            </div>
+            <>
+              <FilterSection
+                handleButtonClick={handleButtonClick}
+                category={category}
+                setCategory={setCategory}
+                minPrice={minPrice}
+                setMinPrice={setMinPrice}
+                maxPrice={maxPrice}
+                setMaxPrice={setMaxPrice}
+                minReviews={minReviews}
+                setMinReviews={setMinReviews}
+                minRating={minRating}
+                setMinRating={setMinRating}
+              />
+            </>
           )}
 
           <div className='z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex my-10'>
