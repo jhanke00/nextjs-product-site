fff6a98afbf9fb9b606ff45bf64d39b5f9f4538e
Ramasamy <2236143@cognizant.com>
2024-07-09 14:50:49 +0530

    home-assessment

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -6,13 +6,13 @@ export default function BreadCrumb({ breadcrumbs }: any) {
   return (
     <div className='flex items-center gap-2 pb-4 text-blue-600 font-bold text-lg'>
       {breadcrumbs.map((list: any, idx: number) => {
-        const isAppendDevider = idx + 1 !== breadcrumbs.length;
+        const isAppendDivider = idx + 1 !== breadcrumbs.length;
         return (
           <div key={idx} className='flex gap-2'>
             <div className={`${list.active ? 'text-blue-900' : 'text-blue-600'}`}>
               <Link href={list.href}>{list.label}</Link>
             </div>
-            {isAppendDevider && <div>/</div>}
+            {isAppendDivider && <div>/</div>}
           </div>
         );
       })}
