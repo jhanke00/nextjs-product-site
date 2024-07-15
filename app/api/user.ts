ad292bffd2eadd39d266caac15035fc8aadf15ab
anuBabu2503 <anu.babu@cognizant.com>
2024-07-15 19:15:14 +0530

    added endpoint to calculate order total

--- app/api/user.ts
+++ app/api/user.ts
@@ -1,5 +1,3 @@
-/*file name: user.ts
-This typescript file is used to fetch the user details from both large dataset and small dataset*/
 import express, { Express, Request, Response } from 'express';
 import fs from 'fs';
 export function userRoutes(app: express.Application) {
@@ -23,8 +21,23 @@ export function userRoutes(app: express.Application) {
       const ordersData = JSON.parse(fs.readFileSync('./src/mock/small/orders.json', 'utf8'));
       const data = [...ordersData];
       const orders = data.filter((order: any) => order.user === userId);
+      return res.status(200).json({ data: { orders: orders }, message: 'Success' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Server error' });
+    }
+  });
+  app.get('/user/order/total/:id', (req: Request, res: Response) => {
+    try {
+      let userId: string = req.params.id; // Assuming userId is passed as a route parameter
+      const ordersData = JSON.parse(fs.readFileSync('./src/mock/small/orders.json', 'utf8'));
+      const data = [...ordersData];
+      const orders = data.filter((order: any) => order.user === userId);
+      if (orders.length === 0) {
+        return res.status(404).json({ message: 'No orders found for this user' });
+      }
       const totalSpent = data.reduce((acc, order) => acc + order.total, 0);
-      return res.status(200).json({ data: { orders: orders, total_spent: totalSpent }, message: 'Success' });
+      return res.status(200).json({ data: { total_spent: totalSpent }, message: 'Success' });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Server error' });
