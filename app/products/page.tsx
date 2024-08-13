e87bb238852477790857b0906e12357cb1dc0ee7
Wilson Cordeiro <wilson.cordeiro@accenture.com>
2024-08-13 11:19:07 -0300

    chore: Refactor CSS classes and add star rating component. Add card behaviour to show stock quantity if less than 10 items and disable card is out of stock

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import largeData from '@/src/mock/large/products.json';
 import smallData from '@/src/mock/small/products.json';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import StarRating from '../../src/components/star-rating';
 
 const PAGE_SIZE = 20;
 
@@ -13,6 +14,14 @@ export default function Products() {
   const endIndex = startIndex + PAGE_SIZE;
   const productData = data.slice(startIndex, endIndex);
   const totalPages = Math.ceil(data.length / PAGE_SIZE);
+
+  const formatPrice = (price: string) => {
+    const priceNumber = parseFloat(price);
+    if (isNaN(priceNumber)) {
+      return 'Invalid price';
+    }
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(priceNumber);
+  };
 
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -27,22 +36,35 @@ export default function Products() {
   }, [currentPage]);
 
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <div className='grid lg:max-w-5xl lg:w-full lg:grid-cols-2 lg:text-left'>
+    <main className='flex min-h-screen flex-col items-center'>
+      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm mb-20 lg:flex'>
+        <div className='grid lg:max-w-5xl lg:w-full lg:grid-cols-2 lg:text-left lg:gap-6'>
           {productData.map((product) => (
             <div
               key={product.id}
-              className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+              className={`flex group rounded-lg border px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ${product.countInStock === 0 ? 'opacity-40' : ''}`}
             >
-              <Link href={`/products/${product.id}`}>
-                <h3 className={`mb-3 text-2xl font-semibold`}>{product.name}</h3>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Price: {product.price}</p>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Description: {product.description}</p>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Category: {product.category}</p>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rating: {product.rating}</p>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Reviews: {product.numReviews}</p>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Stock: {product.countInStock}</p>
+              <Link
+                href={product.countInStock === 0 ? '#' : `/products/${product.id}`}
+                className={`flex flex-col flex-auto ${product.countInStock === 0 ? 'cursor-default' : ''}`}
+                onClick={(e) => product.countInStock === 0 && e.preventDefault()}
+              >
+                <div className='flex-auto'>
+                  <h3 className={`text-2xl font-semibold`}>{product.name}</h3>
+                  <h4 className={`mb-3 text-md opacity-80`}>{product.category}</h4>
+                  <p className={`mb-6 m-0 text-sm opacity-50`}>Description: {product.description}</p>
+                  {product.countInStock === 0 ? (
+                    <span className='bg-red-500 flex justify-end pr-1'>Out of stock</span>
+                  ) : product.countInStock > 1 && product.countInStock < 10 ? (
+                    <span className='bg-red-500 flex justify-end pr-1'>Only {product.countInStock} left in stock!</span>
+                  ) : null}
+                </div>
+                <div className='flex items-center justify-right min-h-10'>
+                  {product.countInStock !== 0 ? (
+                    <span className={`m-0 opacity-50 font-bold text-2xl`}>{formatPrice(product.price)}</span>
+                  ) : null}
+                  <StarRating rating={product.rating} />
+                </div>
               </Link>
             </div>
           ))}
---
+++ src/components/star-rating.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+interface StarRatingProps {
+  rating: number;
+}
+
+const StarRating: React.FC<StarRatingProps> = ({ rating }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const getStarPercentage = (index: number) => {
+    const starValue = index + 1;
+    if (rating >= starValue) {
+      return 100;
+    } else if (rating < starValue - 1) {
+      return 0;
+    } else {
+      return (rating - Math.floor(rating)) * 100;
+    }
+  };
+
+  const stars = [];
+
+  for (let i = 0; i < 5; i++) {
+    const percentage = getStarPercentage(i);
+    stars.push(
+      <div key={i} className='relative text-2xl'>
+        <span className='filled top-0 left-0 text-yellow-500' style={{ width: `${percentage}%` }}>
+          &#9733;
+        </span>
+        <span className='text-gray-300'>&#9733;</span>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className='star-rating flex items-center'
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      {hovered ? <span className='rating-value text-sm opacity-50'>({rating.toFixed(2)})</span> : stars}
+    </div>
+  );
+};
+
+export default StarRating;
