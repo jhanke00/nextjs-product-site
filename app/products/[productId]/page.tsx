f8be9f1991383438a5011d53d13bef7325a2bf96
geazi.e.oliveira <geazi.e.oliveira@accenture.com>
2024-08-23 15:40:54 -0300

    Add similar products functionality and UI improvements
    Implemented a function to find and display similar products based on various criteria. Updated the product details page with better styling, star ratings, and integrated the similar products section. Also, included configuration for remote images compatibility.

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,23 +1,54 @@
 import largeData from '@/src/mock/large/products.json';
 import smallData from '@/src/mock/small/products.json';
+import Image from 'next/image';
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+import SimilarProducts from '@/app/products/similar.products';
+import { Product } from '@type/products';
 
 const productDetail = ({ params }: { params: { productId: string } }) => {
-  const data = [...largeData, ...smallData];
+  const data: Product[] = [...largeData, ...smallData] as unknown as Product[];
   const product = data.find((item) => item.id === params.productId);
   if (!product) {
     return <p>Product not Found</p>;
   }
 
   return (
-    <div className='flex min-h-screen flex-col p-24'>
-      <h1 className='text-2xl font-semibold'>Product Description</h1>
-      <h3 className={`mb-3 text-xl `}>{product.name}</h3>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Price: {product.price}</p>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Description: {product.description}</p>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Category: {product.category}</p>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Rating: {product.rating}</p>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Reviews: {product.numReviews}</p>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Stock: {product.countInStock}</p>
+    <div className='flex min-h-screen flex-col p-8'>
+      <div className='max-w-4xl mx-auto p-6 rounded-lg shadow-md border border-gray-700 bg-gray-800'>
+        <div className='flex flex-col md:flex-row'>
+          <div className='md:w-1/2'>
+            <Image
+              src='https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5b0981ff-45f8-40c3-9372-32430a62aaea/dunk-high-womens-shoes-LwCxXJ.png'
+              alt={product.name}
+              width={500}
+              height={500}
+              className='rounded-lg'
+            />
+          </div>
+          <div className='md:w-1/2 md:pl-8 mt-4 md:mt-0'>
+            <h3 className='mb-3 text-3xl font-semibold text-white'>{product.name}</h3>
+            <p className='m-0 text-lg text-gray-300 font-bold'>${product.price}</p>
+            <p className='mt-2 text-sm text-gray-400'>{product.description}</p>
+            <span className='inline-block mt-3 px-2 py-1 text-xs font-semibold text-white bg-blue-600 rounded'>
+              {product.category}
+            </span>
+            <div className='mt-3 flex items-center'>
+              <div className='flex items-center'>
+                {Array.from({ length: 5 }, (_, index) => (
+                  <FaStar key={index} className={`text-yellow-400`} />
+                ))}
+              </div>
+              <span className='ml-2 text-sm text-gray-400'>({product.numReviews} reviews)</span>
+            </div>
+            <div className='mt-3 text-sm text-gray-400'>
+              <p>Stock: {product.countInStock}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <SimilarProducts products={data} currentProduct={product} />
     </div>
   );
 };
--- app/products/list.tsx
+++ app/products/list.tsx
@@ -6,6 +6,7 @@ import { useFilter } from '@/app/contexts/product-filter';
 import Link from 'next/link';
 import { Product } from '@type/products';
 import ReactPaginate from 'react-paginate';
+import { FaStar } from 'react-icons/fa';
 
 const PAGE_SIZE = 21;
 
@@ -76,14 +77,7 @@ const ProductList: React.FC = () => {
                   <div className='mt-3 flex items-center'>
                     <div className='flex items-center'>
                       {Array.from({ length: 5 }, (_, index) => (
-                        <svg
-                          key={index}
-                          className={`w-4 h-4 ${index < product.rating ? 'text-yellow-400' : 'text-gray-500'}`}
-                          fill='currentColor'
-                          viewBox='0 0 20 20'
-                        >
-                          <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.97a1 1 0 00.95.69h4.18c.969 0 1.371 1.24.588 1.81l-3.39 2.46a1 1 0 00-.364 1.118l1.287 3.97c.3.921-.755 1.688-1.54 1.118l-3.39-2.46a1 1 0 00-1.175 0l-3.39 2.46c-.784.57-1.838-.197-1.54-1.118l1.287-3.97a1 1 0 00-.364-1.118l-3.39-2.46c-.783-.57-.38-1.81.588-1.81h4.18a1 1 0 00.95-.69l1.286-3.97z' />
-                        </svg>
+                        <FaStar key={index} className={`text-yellow-400`} />
                       ))}
                     </div>
                     <span className='ml-2 text-sm text-gray-400'>({product.numReviews} reviews)</span>
