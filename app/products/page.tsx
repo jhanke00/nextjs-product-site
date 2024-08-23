6747be652dfb16f66f00d99fcf19f0de25941a12
geazi.e.oliveira <geazi.e.oliveira@accenture.com>
2024-08-23 15:44:52 -0300

    Add Suspense to wrap entire Products component
    Introduced React's Suspense component to wrap the Products component. This change ensures that asynchronous data fetching or lazy-loaded components can be handled efficiently during the rendering phase.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,18 +2,21 @@
 import { FilterProvider } from '@/app/contexts/product-filter';
 import FilterSidebar from './filter.sidebar';
 import ProductList from '@/app/products/list';
+import { Suspense } from 'react';
 
 export default function Products() {
   return (
-    <FilterProvider>
-      <main className='flex min-h-screen flex-col items-center p-24'>
-        <div className='flex w-full'>
-          <aside className='w-1/4 px-4'>
-            <FilterSidebar />
-          </aside>
-          <ProductList />
-        </div>
-      </main>
-    </FilterProvider>
+    <Suspense>
+      <FilterProvider>
+        <main className='flex min-h-screen flex-col items-center p-24'>
+          <div className='flex w-full'>
+            <aside className='w-1/4 px-4'>
+              <FilterSidebar />
+            </aside>
+            <ProductList />
+          </div>
+        </main>
+      </FilterProvider>
+    </Suspense>
   );
 }
