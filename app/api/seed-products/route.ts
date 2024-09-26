a89471acf5da34d5d56ee43ac310abfe889a16f9
Augusto Bernardes <augustobdev@outlook.com>
2024-09-26 10:44:50 -0300

    feature: implementing repositories and seed for test purpose

---
+++ app/api/seed-products/route.ts
@@ -0,0 +1,12 @@
+// pages/api/hello.ts
+
+import { seedProductsTable } from '@/scripts/seed-products';
+import {  NextResponse } from 'next/server';
+
+
+export async function POST() {
+  const response = await seedProductsTable();
+  return NextResponse.json({
+    response 
+  });
+}
---
+++ src/data/factories/products-db-repository-factory.ts
@@ -0,0 +1,8 @@
+import { ProductsMongoRepository } from '@/infra/db/mongodb/products/products-mongo-repository';
+import { ProductsDbRepository } from '../usecases/products-repository';
+
+export const makeProductsDbRepository = () => {
+
+  const mongoProductsRepository = new ProductsMongoRepository();
+  return new ProductsDbRepository(mongoProductsRepository);
+};
