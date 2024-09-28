1bf7bf5baed2dcc35bff419335c36523aa4a14a5
Augusto Bernardes <augustobdev@outlook.com>
2024-09-27 22:06:15 -0300

    fix: removing duplicated util

--- src/utils/pagination-input-util.ts
+++ src/utils/pagination-input-util.ts
@@ -14,18 +14,3 @@ export class PaginationInputUtil {
     this.skip = (this.page - 1) * this.limit;
   }
 }
-
-export function paginateResponse<T>(data: { result: T; total: number }, page = 1, limit = 10) {
-  const { result, total } = data;
-  limit = Number(limit);
-  page = Number(page);
-  const lastPage = Math.ceil(total / limit);
-  return {
-    total,
-    totalRecords: total,
-    lastPage: lastPage,
-    currentPage: page,
-    firstPage: 1,
-    data: result,
-  };
-}
