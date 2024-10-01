19e3ce0f6effbf70133d3d5348340251d0b2c5e1
guigallo <guilherme.gg1@hotmail.com>
2024-10-01 16:09:36 -0300

    refactor: move docker to infra, add default secret to pass on ci

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,12 +1,13 @@
 import './envConfig';
 import jwt from 'jsonwebtoken';
 
-const secretKey: string = process.env.JWT_SECRET!;
+const secretKey: string = process.env.JWT_SECRET! || 'secret';
 const signOptions: jwt.SignOptions | undefined = { expiresIn: '1h' };
 const saltOrRounds: number | string = 10;
 
-if (!secretKey) {
-  throw new Error('Please define the JWT_SECRET environment variable');
+if (secretKey === 'secret') {
+  console.warn('Warning: Using default secret key, please define JWT_SECRET environment variable');
+  // throw new Error('Please define the JWT_SECRET environment variable');
 }
 
 export { secretKey, signOptions, saltOrRounds };
