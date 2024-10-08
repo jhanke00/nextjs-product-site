234421d4cf07bc5bdb5a176a7f49edd6c587d431
brunomanfrinato <bru.manfrinato@gmail.com>
2024-10-08 10:58:48 -0300

    Fix issue on coundInStock field for products

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -37,7 +37,7 @@ const productSchema = new mongoose.Schema<IProductDocument>({
     type: Number,
     default: 0,
   },
-  countOnStock: {
+  countInStock: {
     type: Number,
     default: 0,
     min: 0,
