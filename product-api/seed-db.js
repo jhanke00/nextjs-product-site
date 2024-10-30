f3b650d5c07e14ce6fa5a4b6caaa95ecbe76503c
André Mendonça e Silva <dexmendoncadev@gmail.com>
2024-10-30 17:32:53 -0300

    refactor: refactor seed

--- product-api/seed-db.js
+++ product-api/seed-db.js
@@ -7,19 +7,47 @@ mongoose
   .then(() => console.log('[MongoDB] Connected to MongoDB'))
   .catch((err) => console.error('[MongoDB] Failed to Connect', err));
 
-const importData = async (model, dataPath, name) => {
-  const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-  await model.insertMany(data);
-  console.log(`[MongoDB] Import collection '${name}' from '${dataPath}'`);
-};
-
-const usersSchema = new mongoose.Schema({}, { strict: false });
+const usersSchema = new mongoose.Schema(
+  {
+    _id: String,
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    email: String,
+  },
+  { _id: false, versionKey: false },
+);
 const Users = mongoose.model('users', usersSchema);
 
-const productsSchema = new mongoose.Schema({}, { strict: false });
+const productsSchema = new mongoose.Schema(
+  {
+    _id: String,
+    name: String,
+    price: String,
+    description: String,
+    category: String,
+    rating: Number,
+    numReviews: Number,
+    countInStock: Number,
+  },
+  { _id: false, versionKey: false },
+);
 const Products = mongoose.model('products', productsSchema);
 
-const ordersSchema = new mongoose.Schema({}, { strict: false });
+const ordersSchema = new mongoose.Schema(
+  {
+    user: { type: String, ref: 'User' },
+    items: [
+      {
+        product: { type: String, ref: 'Product' },
+        count: Number,
+      },
+    ],
+    total: Number,
+    time: Date,
+  },
+  { versionKey: false },
+);
 const Orders = mongoose.model('orders', ordersSchema);
 
 const setupDatabase = async () => {
@@ -28,11 +56,34 @@ const setupDatabase = async () => {
     await Products.deleteMany();
     await Orders.deleteMany();
 
-    await importData(Users, './db/users.json', 'users');
-    await importData(Products, './db/products.json', 'products');
-    await importData(Orders, './db/orders.json', 'orders');
+    const usersData = JSON.parse(fs.readFileSync('./db/users.json', 'utf8'));
+    const productsData = JSON.parse(
+      fs.readFileSync('./db/products.json', 'utf8'),
+    );
+    const ordersData = JSON.parse(fs.readFileSync('./db/orders.json', 'utf8'));
 
-    console.log('[MongoDB] Data imported!');
+    const users = usersData.map((user) => ({ _id: user.id, ...user }));
+    await Users.insertMany(users);
+    console.log('Users importados com sucesso.');
+
+    const products = productsData.map((product) => ({
+      _id: product.id,
+      ...product,
+    }));
+    await Products.insertMany(products);
+    console.log('Products importados com sucesso.');
+
+    const orders = ordersData.map((order) => ({
+      user: order.user,
+      items: order.items.map((item) => ({
+        product: item.id,
+        count: item.count,
+      })),
+      total: order.total,
+      time: new Date(order.time),
+    }));
+    await Orders.insertMany(orders);
+    console.log('Orders importados com sucesso.');
   } catch (error) {
     console.error('[MongoDB] Failed on seed data', error);
   } finally {
