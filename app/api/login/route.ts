17153fe2d7d0fee40b8e3b098bcde0565fc6297e
Gabriel Elias <77940881+helayas@users.noreply.github.com>
2024-11-18 10:21:48 -0300

    fix: fix dependency typo

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { connectToDatabase } from '@/infra/db/mongo/mongo';
 import User from '@/infra/db/mongo/models/User';
